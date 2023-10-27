6556cbaaecb95b4f8371e1cdbe95ec99bb03c76f
michaeljymsgutierrez <leeez711@gmail.com>
2023-10-27 15:13:23 +0800

    Added build directory and gitignore

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-zip -r fps-booster fps-booster/ && echo "Build finished!" || echo "Build failed!"
-zip -r fps-unlocker fps-unlocker/ && echo "Build finished!" || echo "Build failed!"
-zip -r v2-fast-charging v2-fast-charging/ && echo "Build finished!" || echo "Build failed!"
+mkdir build
+zip -r build/fps-booster fps-booster/ && echo "Build finished!" || echo "Build failed!"
+zip -r build/fps-unlocker fps-unlocker/ && echo "Build finished!" || echo "Build failed!"
+zip -r build/v2-fast-charging v2-fast-charging/ && echo "Build finished!" || echo "Build failed!"
