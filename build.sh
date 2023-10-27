b4b3cc031beaf579f472b1e0a8b9fe46e6117818
michaeljymsgutierrez <leeez711@gmail.com>
2023-10-27 15:10:11 +0800

    Added build script

---
+++ build.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+zip -r fps-booster fps-booster/ && echo "Build finished!" || echo "Build failed!"
+zip -r fps-unlocker fps-unlocker/ && echo "Build finished!" || echo "Build failed!"
+zip -r v2-fast-charging v2-fast-charging/ && echo "Build finished!" || echo "Build failed!"
